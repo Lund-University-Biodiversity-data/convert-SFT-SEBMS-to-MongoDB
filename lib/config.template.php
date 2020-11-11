d602d7e4b53765f4b800263d56b75369e1297606
mathieuLU <you@example.com>
2020-11-11 16:46:35 +0100

    config template

--- lib/config.template.php
+++ lib/config.template.php
@@ -1,6 +1,64 @@
 <?php
-$DB["host"]="localhost";
-$DB["username"]="postgres";
-$DB["database"]="sft";
-$DB["password"]="";
+$commonFields=array();
+
+$commonFields["userId"]=0;
+
+$commonFields["status"]="active";
+$commonFields["recordedBy"]="";
+$commonFields["rightsHolder"]="";
+$commonFields["institutionID"]=$commonFields["rightsHolder"];
+$commonFields["institutionCode"]=$commonFields["rightsHolder"];
+$commonFields["basisOfRecord"]="HumanObservation";
+$commonFields["multimedia"]="[ ]";
+$commonFields["licence"]="https://creativecommons.org/publicdomain/zero/1.0/";
+
+switch ($database) {
+	case "SFT":
+		$DB["host"]="localhost";
+		$DB["username"]="postgres";
+		$DB["database"]="sft";
+		$DB["password"]="";
+
+
+		$commonFields["listSpeciesId"]["birds"]="dr158";
+		$commonFields["listSpeciesId"]["mammals"]="dr159";
+		$commonFields["listSpeciesId"]["amphibians"]="dr160";
+		$commonFields["listSpeciesId"]["owls"]="dr161";
+
+		// STANDARDRUTTERNA
+		$commonFields["std"]["projectId"]="";
+		$commonFields["std"]["projectActivityId"]="";
+		$commonFields["std"]["datasetId"]=$commonFields["std"]["projectActivityId"];
+		$commonFields["std"]["datasetName"]=""; // survey name
+		$commonFields["std"]["type"]=""; // activity form
+		$commonFields["std"]["name"]=""; // form section
+
+		// NATTRUTTERNA
+		// same as std with natt / vinter / sommar
+
+		break;
+	case "SEBMS":
+		$DB["host"]="localhost";
+		$DB["username"]="postgres";
+		$DB["database"]="sebms";
+		$DB["password"]="";
+
+		$commonFields["listSpeciesId"]="dr156";
+
+		// PUNKTLOKAL
+		$commonFields["punktlokal"]["projectId"]="";
+		$commonFields["punktlokal"]["projectActivityId"]="";
+		$commonFields["punktlokal"]["datasetId"]=$commonFields["punktlokal"]["projectActivityId"];
+		$commonFields["punktlokal"]["datasetName"]=""; // survey name
+		$commonFields["punktlokal"]["datasourceID"]=[];
+		$commonFields["punktlokal"]["type"]=""; // activity form
+		$commonFields["punktlokal"]["name"]=""; // form section
+
+		// SLINGA
+
+		break;
+}
+
+
+
 ?>
