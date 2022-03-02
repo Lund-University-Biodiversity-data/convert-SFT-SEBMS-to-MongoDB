1c64e5601b9d060eaeff337fb764059395703535
mathieuLU <you@example.com>
2022-03-02 14:07:06 +0100

    script check scientifName

---
+++ script/checkUnknownScientificNames.php
@@ -0,0 +1,97 @@
+<?php
+$database="SFT";
+require dirname(__FILE__)."/../lib/config.php";
+$server=DEFAULT_SERVER;
+require dirname(__FILE__)."/../lib/functions.php";
+require PATH_SHARED_FUNCTIONS."generic-functions.php";
+
+echo consoleMessage("info", "Script starts.");
+echo consoleMessage("info", "php script/editRecordsAddSwedishRank.php");
+
+$mng = new MongoDB\Driver\Manager($mongoConnection[$server]); // Driver Object created
+
+$array_species_sn=array();
+$array_species_guid=array();
+
+foreach ($commonFields["listSpeciesId"] as $animals => $listId) {
+    $url="https://lists.biodiversitydata.se/ws/speciesListItems/".$commonFields["listSpeciesId"][$animals]."?includeKVP=true";
+    $obj = json_decode(file_get_contents($url), true);
+
+    foreach($obj as $sp) {
+        
+        $array_species_sn[$animals][]=$sp["name"];
+
+
+        foreach ($sp["kvpValues"] as $iKvp => $kvp) {
+            if (isset($kvp["key"]) && $kvp["key"]=="rank") {
+                $rank=$kvp["value"];
+                $array_species_rank[$animals][$rank]=$sp["name"];
+            }   
+
+        }
+
+    }
+
+    echo consoleMessage("info", "Species list ".$commonFields["listSpeciesId"][$animals]." obtained. ".count($obj)." elements");
+
+}
+//print_r($array_species_sn["birds"]);
+//echo $array_species_rank["birds"][3090]; exit;
+
+// get all the sites
+$filter = ["status"=>"active", "name" => $commonFields["std"]["name"]];
+//$filter = ["dataOrigin" => "scriptSitePunktIntranet", "verificationStatus"=>"godkänd", "status"=>"active"];
+$options = [];
+/*$options = [
+   'limit' => 100,
+]; */
+$query = new MongoDB\Driver\Query($filter, $options); 
+
+$rows = $mng->executeQuery("ecodata.output", $query);
+
+$nbSNok=0;
+$nbSNfixed=0;
+$nbSNerror=0;
+$nbSNunknown=0;
+foreach ($rows as $row){
+
+    $animals="birds";
+    if (isset($row->data->observations)) {
+
+        $dateCreated="";
+        foreach($row->data->observations as $obs) {
+
+            if (in_array($obs->species->scientificName, $array_species_sn[$animals] )) {
+                $nbSNok++;
+            }
+            else {
+                //echo consoleMessage("error", "Can't find SN ".$obs->species->scientificName." from activity ".$row->activityId." in array");
+                $nbSNerror++;
+                $nbSNunknown++;
+
+                $dateCreated=$row->data->surveyDate;
+
+                if (isset($obs->swedishRank) && isset($array_species_rank[$animals][intval($obs->swedishRank)]) && trim($array_species_rank[$animals][intval($obs->swedishRank)])!="") {
+                    $nbSNfixed++;
+                    //echo consoleMessage("warn", $obs->species->scientificName." replaced by ".$array_species_rank[$animals][intval($obs->swedishRank)]);
+
+                }
+                else {
+                    echo consoleMessage("error", "Can't fix with rank ".intval($obs->swedishRank)." ".$array_species_rank[$animals][intval($obs->swedishRank)]);
+                }
+            }
+                
+        }
+        if ($dateCreated!="") echo consoleMessage("warn", "Problem with activity ".$row->activityId." created ".$dateCreated);
+    }
+    else {
+        echo consoleMessage("warn", "No observations  for ".$row->activityId);
+    }
+}
+
+echo consoleMessage("info", $nbSNok." ok scientificNames.");
+echo consoleMessage("info", $nbSNerror." error scientificNames.");
+echo consoleMessage("info", $nbSNfixed." fixed scientificNames.");
+echo consoleMessage("info", "including ".$nbSNunknown." unknown scientificNames.");
+
+?>
